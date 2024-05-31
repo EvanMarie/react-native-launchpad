471ba3977e44cbe58826e938b945c1db38d8fc60
Evan Marie Carr <evanmariecarr@gmail.com>
2024-05-31 12:46:41 -0400

    update the scroll progress

--- custom-components/scrollProgress.tsx
+++ custom-components/scrollProgress.tsx
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from "react";
 import { View, ScrollView, Animated, StyleSheet } from "react-native";
 import { screenHeight, screenWidth } from "@/constants/variousConstants";
-import { VStackFull } from "./containers";
 import { col } from "@/constants/Colors";
+import { VStackFull } from "./containers";
+import { bordersB } from "@/constants/BorderStyles";
 
 export default function ScrollProgress({
   children,
@@ -11,7 +12,17 @@ export default function ScrollProgress({
   right,
   left,
   bottom,
-  color = col[900],
+  color = col[600],
+  style = {},
+  paddingTop = 50,
+  height = screenHeight * 0.88,
+  progressBackground = col[200],
+  progressWidth = screenWidth,
+  progressHeight = 5,
+  progressBorderRadius = 0,
+  contentPaddingVertical = 10,
+  contentPaddingHorizontal = 10,
+  borderBottom = bordersB.borderBmd400,
 }: {
   children: React.ReactNode;
   position?: "absolute" | "relative";
@@ -20,6 +31,16 @@ export default function ScrollProgress({
   left?: number;
   bottom?: number;
   color?: string;
+  style?: object;
+  paddingTop?: number;
+  height?: number;
+  progressBackground?: string;
+  progressWidth?: number;
+  progressHeight?: number;
+  progressBorderRadius?: number;
+  contentPaddingVertical?: number;
+  contentPaddingHorizontal?: number;
+  borderBottom?: any;
 }) {
   const scrollY = useRef(new Animated.Value(0)).current;
   const [contentHeight, setContentHeight] = useState(0);
@@ -28,45 +49,40 @@ export default function ScrollProgress({
     container: {
       flex: 1,
       position: "relative",
-      paddingTop: 50,
+      paddingTop: paddingTop,
     },
     scrollContainer: {
       flexDirection: "column",
     },
-    content: {
-      width: screenWidth - 40,
-      marginHorizontal: 20,
-      backgroundColor: "lightblue",
-      height: screenHeight * 0.5,
-      borderRadius: 10,
-    },
     progressContainer: {
       height: 5,
-      backgroundColor: "lightgray",
+      backgroundColor: progressBackground,
       width: screenWidth,
       zIndex: 1,
     },
     progressBar: {
-      height: 5,
+      height: progressHeight,
+      width: progressWidth,
       backgroundColor: color,
       position: position,
       top: top,
       left: left,
       right: right,
       bottom: bottom,
+      borderRadius: progressBorderRadius,
     },
   });
 
   return (
-    <View style={styles.container}>
+    <View style={(styles.container, style)}>
       <View style={styles.progressContainer}>
-        {contentHeight > screenHeight && (
+        {contentHeight > height && (
           <Animated.View
             style={[
               styles.progressBar,
               {
                 width: scrollY.interpolate({
-                  inputRange: [0, contentHeight - screenHeight],
+                  inputRange: [0, contentHeight - height],
                   outputRange: [0, screenWidth],
                   extrapolate: "clamp",
                 }),
@@ -84,8 +100,15 @@ export default function ScrollProgress({
         scrollEventThrottle={16}
         contentContainerStyle={styles.scrollContainer}
         onContentSizeChange={(width, height) => setContentHeight(height)}
+        style={[borderBottom, { height: height }]}
       >
-        <VStackFull style={{ gap: 10, paddingVertical: 10 }}>
+        <VStackFull
+          style={{
+            gap: 10,
+            paddingVertical: contentPaddingVertical,
+            paddingHorizontal: contentPaddingHorizontal,
+          }}
+        >
           {children}
         </VStackFull>
       </ScrollView>
